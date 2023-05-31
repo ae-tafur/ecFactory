4aae2c5970d260590ac4a6d46cb6a71227362153
ae-tafur <ae.tafur@hotmail.com>
2023-05-31 21:11:24 +0200

    fix: FVA enzyme usage to GECKO 3

--- code/enzymeUsage_FVA.m
+++ code/enzymeUsage_FVA.m
@@ -6,57 +6,54 @@ function FVAtable = enzymeUsage_FVA(model,enzymes)
 %
 %   Ivan Domenzain.     Last edited 2020-05-27
 
-if nargin<2
-    enzymes = model.enzymes;
+if nargin < 2
+    enzymes = model.ec.enzymes;
 end
+
 %Get parsimonious protein usages
 tempModel  = model;
 pool_indx  = find(strcmpi(model.rxns,'prot_pool_exchange'));
-prot_indxs = find(contains(model.rxnNames,'draw_prot_'));
+
 %prot_indxs = prot_indxs(1:(end-1));
-tempModel = setParam(tempModel, 'obj',pool_indx,-1);
-sol       = solveLP(tempModel,1);
+tempModel = setParam(tempModel, 'obj', pool_indx, 1); % Minimize protein pool
+solUsgs   = solveLP(tempModel, 1);
+
 %initialize variables
-ranges    = zeros(length(enzymes),1);
+enzIDs    = cell(length(enzymes),1);
+enz_pUsgs = zeros(length(enzymes),1);
 minUsgs   = zeros(length(enzymes),1);
 maxUsgs   = zeros(length(enzymes),1);
-enz_pUsgs = zeros(length(enzymes),1);
-enzIDs    = cell(length(enzymes),1);
-if ~isempty(sol.x)
-    pUsgs = sol.x(prot_indxs);
-    %Loop through all the provided enzymes
+ranges    = zeros(length(enzymes),1);
+
+if ~isempty(solUsgs.x)
+    % pUsgs = sol.x(prot_indxs);
+    % Loop through all the provided enzymes
     for i=1:length(enzymes)
         if ~isempty(enzymes{i})
-            minFlux = 0;
-            maxFlux = 0;
-            pUsage  = 0;
-            rxnIndx = find(contains(model.rxnNames,enzymes{i}),1);
-            enzIndx = find(strcmpi(model.enzymes,enzymes{i}),1);
+            rxnIndx = find(strcmpi(model.rxns, strcat('usage_prot_', enzymes{i})));
+            enz_pUsgs(i) = solUsgs.x(rxnIndx);
+            % enzIndx = find(strcmpi(model.ec.enzymes,enzymes{i}),1);
             enzIDs(i)  = enzymes(i);
-            if ~isempty(enzIndx)
-                model  = setParam(model, 'obj', rxnIndx, -1);
+            if ~isempty(rxnIndx)
+                model  = setParam(model, 'obj', rxnIndx, +1);
                 sol    = solveLP(model);
-                pUsage = pUsgs(enzIndx);
                 if ~isempty(sol.f)
-                    minFlux = sol.x(rxnIndx);
-                    model   = setParam(model, 'obj', rxnIndx, +1);
+                    minUsgs(i) = sol.x(rxnIndx);
+                    model   = setParam(model, 'obj', rxnIndx, -1);
                     sol     = solveLP(model);
                     if ~isempty(sol.f)
                         %disp(['Ready with enzyme #' num2str(i) ' ' model.enzymes{enzIndx}])
-                        maxFlux = sol.x(rxnIndx);
+                        maxUsgs(i) = sol.x(rxnIndx);
                     end
                 end
-                ranges(i)    = (maxFlux-minFlux);
-                minUsgs(i)   = minFlux;
-                maxUsgs(i)   = maxFlux;
-                enz_pUsgs(i) = pUsage;
+                ranges(i)    = (minUsgs(i)-maxUsgs(i));
             end
         else
-                ranges(i)    = NaN;
-                minUsgs(i)   = NaN;
-                maxUsgs(i)   = NaN;
-                enz_pUsgs(i) = NaN;
-                %enzIDs{i}    = 'empty';
+            ranges(i)    = NaN;
+            minUsgs(i)   = NaN;
+            maxUsgs(i)   = NaN;
+            enz_pUsgs(i) = NaN;
+            %enzIDs{i}    = 'empty';
         end
     end
 else
