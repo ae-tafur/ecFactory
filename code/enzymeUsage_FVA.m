7d8052df4dcfd393c81c55a88ec4c01097ab17ba
ae-tafur <ae.tafur@hotmail.com>
2023-07-07 13:00:59 +0200

    fix: use RAVEN to get max and min usage

--- code/enzymeUsage_FVA.m
+++ code/enzymeUsage_FVA.m
@@ -1,4 +1,4 @@
-function FVAtable = enzymeUsage_FVA(model,enzymes)
+function fvaTable = enzymeUsage_FVA(model,enzymes)
 % enzymeUsage_FVA
 %   Perf
 %
@@ -10,53 +10,39 @@ if nargin < 2
     enzymes = model.ec.enzymes;
 end
 
-%Get parsimonious protein usages
+% Get protein usage reaction index 
+[~,rxnIdx] = ismember(strcat('usage_prot_', enzymes), model.rxns);
+
+% Initialize variables
+enzIds  = erase(model.rxns(rxnIdx),'usage_prot_'); % Only keep members
+
+% Get parsimonious protein usages
 model = setParam(model, 'obj', 'prot_pool_exchange', 1); % Minimize protein pool
-solUsgs = solveLP(model);
-%poolIdx = find(strcmpi(model.rxns,'prot_pool_exchange'));
-%model = setParam(model, 'lb', 'prot_pool_exchange', solUsgs.x(poolIdx));
+sol = solveLP(model, 1);
 
-%initialize variables
-enzIDs    = cell(length(enzymes),1);
-enzpUsgs  = zeros(length(enzymes),1);
-minUsgs   = zeros(length(enzymes),1);
-maxUsgs   = zeros(length(enzymes),1);
-ranges    = zeros(length(enzymes),1);
+if ~isempty(sol.x)
 
-if ~isempty(solUsgs.x)
-    % pUsgs = sol.x(prot_indxs);
-    % Loop through all the provided enzymes
-    for i=1:length(enzymes)
-        if ~isempty(enzymes{i})
-            rxnIndx = find(strcmpi(model.rxns, strcat('usage_prot_', enzymes{i})));
-            enzpUsgs(i) = solUsgs.x(rxnIndx);
-            % enzIndx = find(strcmpi(model.ec.enzymes,enzymes{i}),1);
-            enzIDs(i)  = enzymes(i);
-            if ~isempty(rxnIndx)
-                model  = setParam(model, 'obj', rxnIndx, +1);
-                sol    = solveLP(model);
-                if ~isempty(sol.f)
-                    minUsgs(i) = sol.x(rxnIndx);
-                    model   = setParam(model, 'obj', rxnIndx, -1);
-                    sol     = solveLP(model);
-                    if ~isempty(sol.f)
-                        %disp(['Ready with enzyme #' num2str(i) ' ' model.enzymes{enzIndx}])
-                        maxUsgs(i) = sol.x(rxnIndx);
-                    end
-                end
-                ranges(i) = (minUsgs(i)-maxUsgs(i));
-            end
-        else
-            ranges(i)    = NaN;
-            minUsgs(i)   = NaN;
-            maxUsgs(i)   = NaN;
-            enzpUsgs(i)  = NaN;
-            %enzIDs{i}    = 'empty';
-        end
-    end
+    % Protein usage
+    enzUsage = sol.x(rxnIdx);
+
+    % Get maximum and minimum protein usage
+    [maxUsage, minUsage, ~] = getAllowedBounds(model, rxnIdx);
+
+    % Use positve numbers
+    enzUsage = abs(enzUsage);
+    minUsage = abs(minUsage);
+    maxUsage = abs(maxUsage);
+
+    % Set values which are under solver tolerance
+    enzUsage(enzUsage < 1e-8) = 0;
+    minUsage(minUsage < 1e-8) = 0;
+    maxUsage(maxUsage < 1e-8) = 1e-8;
+
+    ranges = maxUsage - minUsage;
+
 else
     disp('Model is not feasible')
 end
-varNamesT = {'enzymes' 'ranges' 'minU' 'maxU' 'pU'};
-FVAtable  = table(enzIDs,ranges,minUsgs,maxUsgs,enzpUsgs,'VariableNames', varNamesT);
+varNamesT = {'enzymes' 'ranges' 'minUsage' 'maxUsage' 'enzUsage'};
+fvaTable  = table(enzIds,ranges,minUsage,maxUsage,enzUsage,'VariableNames', varNamesT);
 end
